da09f9ad707ab393bfbc7af5494ca2fe4a97d04a
Dmitry Sky <dmitry.sky@icloud.com>
2015-05-04 15:13:58 -0600

    challenge complete

--- flatten.rb
+++ flatten.rb
@@ -5,12 +5,18 @@ class CustomArray
     @array = array
   end
 
-  def flatten
+  def hacky_flatten
     array.join.chars.map(&:to_i)
+  end
+
+  def flatten
+    array.to_s.delete("][").split(',').map(&:to_i)
   end
 end
 
 
 c = CustomArray.new([[1,2],[3,[4,5]]])
-c.flatten
+c.hacky_flatten
 
+d = CustomArray.new([[12,2],[3,[4,5]]])
+d.flatten
